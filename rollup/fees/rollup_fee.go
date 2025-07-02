452c7d61ef3dff3ffa6e008e34a5f1833663035c
georgehao <haohongfan@gmail.com>
2025-07-02 16:47:31 +0800

    print compression ration

--- rollup/fees/rollup_fee.go
+++ rollup/fees/rollup_fee.go
@@ -207,6 +207,8 @@ func estimateTxCompressionRatio(data []byte, blockNumber uint64, blockTime uint6
 	ratio := new(big.Int).Mul(originalSize, rcfg.Precision)
 	ratio.Div(ratio, compressedSize)
 
+	log.Info("Estimated compression ratio", "ratio", ratio, "original size", originalSize, "compressed size", compressedSize, "data size", len(data), "data", common.Bytes2Hex(data))
+
 	return ratio, nil
 }
 
